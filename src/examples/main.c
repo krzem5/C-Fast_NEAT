1027aac1e68f5dcf6a7dbd583ccbb36cb7d76dcb
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-01-09 18:07:54 +0100

    Push Update 09/01/2023, 18:07:54

--- src/examples/main.c
+++ src/examples/main.c
@@ -9,11 +9,13 @@
 
 
 
+DECLARE_EXAMPLE_FN(xor2);
 DECLARE_EXAMPLE_FN(xor3);
 
 
 
 static const example_t _example_data[]={
+	DECLARE_EXAMPLE(xor2,2,1,500,250,0.999f),
 	DECLARE_EXAMPLE(xor3,3,1,500,250,0.999f),
 	{
 		NULL
---
+++ src/examples/xor2.c
@@ -0,0 +1,28 @@
+#include <math.h>
+#include <neat.h>
+#include <stdio.h>
+
+
+
+float xor2_fitness_score_callback(const neat_t* neat,const neat_genome_t* genome){
+	float out=0.0f;
+	for (unsigned int i=0;i<4;i++){
+		float genome_in[2]={i&1,i>>1};
+		float genome_out;
+		neat_genome_evaluate(neat,genome,genome_in,&genome_out);
+		float diff=genome_out-((i&1)^(i>>1));
+		out+=diff*diff;
+	}
+	return 1/(1+sqrtf(out));
+}
+
+
+
+void xor2_end_callback(const neat_t* neat,const neat_genome_t* genome){
+	for (unsigned int i=0;i<4;i++){
+		float genome_in[2]={i&1,i>>1};
+		float genome_out;
+		neat_genome_evaluate(neat,genome,genome_in,&genome_out);
+		printf("%u^%u=%.3f\n",i&1,i>>1,genome_out);
+	}
+}
