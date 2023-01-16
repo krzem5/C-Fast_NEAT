d26b8fdd809a60de1b0c0107e63a55e29949218c
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-01-16 18:52:20 +0100

    Push Update 16/01/2023, 18:52:20

--- src/fast_neat/neat.c
+++ src/fast_neat/neat.c
@@ -5,12 +5,11 @@
 
 
 
-#define BREED_MUTATION_CHANCE 0.5f
-#define NODE_ADD_CHANCE 0.02f
-#define WEIGHT_ADJUST_CHANCE 0.4f
-#define WEIGHT_SET_CHANCE 0.1f
-#define BIAS_ADJUST_CHANCE 0.3f
-#define BIAS_SET_CHANCE 0.1f
+#define NODE_ADD_CHANCE 2
+#define WEIGHT_ADJUST_CHANCE 40
+#define WEIGHT_SET_CHANCE 10
+#define BIAS_ADJUST_CHANCE 30
+#define BIAS_SET_CHANCE 10
 #define MAX_STALE_FITNESS_DIFFERENCE 1e-6f
 
 
@@ -31,14 +30,20 @@ typedef struct _NEAT_MODEL_FILE_EDGE{
 
 
 
-static inline float _random_uniform(void){
-	return ((float)rand())/RAND_MAX;
+static inline unsigned int _random_uint32(void){
+	return (rand()&0xff)|((rand()&0xff)<<8)|((rand()&0xff)<<16)|((rand()&0xff)<<24);
+}
+
+
+
+static inline float _random_uniform_rescaled(void){
+	return (float)(((int32_t)_random_uint32())>>7)*0x1p-24f;
 }
 
 
 
 static inline unsigned int _random_int(unsigned int max){
-	return rand()%max;
+	return _random_uint32()%max;
 }
 
 
@@ -70,7 +75,7 @@ void neat_init(unsigned int input_count,unsigned int output_count,unsigned int p
 		for (unsigned int j=0;j<node_count;j++){
 			(genome->nodes+j)->bias=0.0f;
 			for (unsigned int k=0;k<node_count;k++){
-				(genome->edges+l)->weight=_random_uniform()*2-1;
+				(genome->edges+l)->weight=_random_uniform_rescaled();
 				l++;
 			}
 		}
@@ -168,9 +173,9 @@ const neat_genome_t* neat_update(neat_t* neat,float (*fitness_score_callback)(co
 	neat_genome_t* child=start_genome;
 	for (unsigned int idx=(start_genome-neat->genomes);idx<neat->population;idx++){
 		const neat_genome_t* random_genome=neat->genomes+_random_int(idx);
-		if (stale||_random_uniform()<=BREED_MUTATION_CHANCE){
-			float value=_random_uniform()*(NODE_ADD_CHANCE+WEIGHT_ADJUST_CHANCE+WEIGHT_SET_CHANCE+BIAS_ADJUST_CHANCE+BIAS_SET_CHANCE);
-			_Bool add_node=value<=NODE_ADD_CHANCE;
+		if (stale||_random_int(2)){
+			unsigned int action=_random_int(NODE_ADD_CHANCE+WEIGHT_ADJUST_CHANCE+WEIGHT_SET_CHANCE+BIAS_ADJUST_CHANCE+BIAS_SET_CHANCE);
+			_Bool add_node=action<=NODE_ADD_CHANCE;
 			_adjust_genome_node_count(child,random_genome->node_count+add_node);
 			if (add_node){
 				if (child->node_count>neat->_evaluation_buffer_size){
@@ -191,7 +196,7 @@ const neat_genome_t* neat_update(neat_t* neat,float (*fitness_score_callback)(co
 					}
 					for (unsigned int j=0;j<child->node_count;j++){
 						if (i==insert_index||j==insert_index){
-							(child->edges+k)->weight=_random_uniform()*2-1;
+							(child->edges+k)->weight=_random_uniform_rescaled();
 						}
 						else{
 							(child->edges+k)->weight=random_genome_edge->weight;
@@ -210,17 +215,17 @@ const neat_genome_t* neat_update(neat_t* neat,float (*fitness_score_callback)(co
 						k++;
 					}
 				}
-				if (value<=NODE_ADD_CHANCE+WEIGHT_ADJUST_CHANCE){
-					(child->edges+_random_int(random_genome->node_count*random_genome->node_count))->weight+=_random_uniform()*2-1;
+				if (action<=NODE_ADD_CHANCE+WEIGHT_ADJUST_CHANCE){
+					(child->edges+_random_int(random_genome->node_count*random_genome->node_count))->weight+=_random_uniform_rescaled();
 				}
-				else if (value<=NODE_ADD_CHANCE+WEIGHT_ADJUST_CHANCE+WEIGHT_SET_CHANCE){
-					(child->edges+_random_int(random_genome->node_count*random_genome->node_count))->weight=_random_uniform()*2-1;
+				else if (action<=NODE_ADD_CHANCE+WEIGHT_ADJUST_CHANCE+WEIGHT_SET_CHANCE){
+					(child->edges+_random_int(random_genome->node_count*random_genome->node_count))->weight=_random_uniform_rescaled();
 				}
-				else if (value<=NODE_ADD_CHANCE+WEIGHT_ADJUST_CHANCE+WEIGHT_SET_CHANCE+BIAS_ADJUST_CHANCE){
-					(child->nodes+_random_int(random_genome->node_count))->bias+=_random_uniform()*2-1;
+				else if (action<=NODE_ADD_CHANCE+WEIGHT_ADJUST_CHANCE+WEIGHT_SET_CHANCE+BIAS_ADJUST_CHANCE){
+					(child->nodes+_random_int(random_genome->node_count))->bias+=_random_uniform_rescaled();
 				}
 				else{
-					(child->nodes+_random_int(random_genome->node_count))->bias=_random_uniform()*2-1;
+					(child->nodes+_random_int(random_genome->node_count))->bias=_random_uniform_rescaled();
 				}
 			}
 		}
